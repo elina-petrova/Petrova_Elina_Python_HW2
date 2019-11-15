1e43fa4c6217c205d945d29d456cb3878eda470e
Petrova <e_petrova@ats-hr420-dm33.fc.ca>
2019-11-14 20:55:56 -0500

    code

---
+++ game.py
@@ -0,0 +1,35 @@
+from random import randint
+from gameFunctions import winlose
+from gameFunctions import compare
+from gameFunctions import gameVars
+
+
+while gameVars.player is False:
+	print("**********************************")
+	print("Computer lives: ", gameVars.computer_lives, "/" , gameVars.total_lives, "\n")
+	print("Player lives: ", gameVars.player_lives, "/",gameVars.total_lives, "\n")
+	print("Choose your weapon!\n")
+	print("**********************************")
+
+	gameVars.player = input("choose rock, paper or scissors: ")
+	gameVars.player = gameVars.player.lower()
+
+	print("computer chose ", gameVars.computer, "\n")
+	print("player chose ", gameVars.player, "\n")
+
+	compare.comparison(gameVars.player, gameVars.computer)
+
+
+
+
+	# handle all lives lost for player or AI
+	if gameVars.player_lives == 0:
+		winlose.winorlose("lost")
+
+	elif gameVars.computer_lives == 0:
+		winlose.winorlose("won")
+
+	else:
+		# need to check all of our conditions after checking for a tie
+		gameVars.player = False
+		gameVars.computer = gameVars.choices[randint(0, 2)]	
